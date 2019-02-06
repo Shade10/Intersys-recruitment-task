940d6d308ec128ad9887fc68d0ed40cdc9671af8
Marcin Kass <marcin.kass10@gmail.com>
2019-02-06 23:49:23 +0100

    fixed task - no NaN text at no results

--- src/components/FixAge.js/FixAge.js
+++ src/components/FixAge.js/FixAge.js
@@ -2,35 +2,55 @@ import React, { Component } from "react";
 
 import "./FixAge.css";
 
-// Write a program that takes an integer N(N between 2 and 20 inclusive), and an array of N integers as input.Your program should output the most occurring digit that appears in the numbers of this array.If more than one digit satisfies this condition, output the bigger one.Note: The integer N will be given in the first line, and the array of N integers will be given in the second line.Each integer will be separated by a single space character ' '.Note2: A digit is an integer between 0 and 9. Example: Case 1: For input given as: 4 101 20 21 3 The output of the program will be: 1 Description of the output: The digit 0 appears 2 times(One time on the first number and another time on the second number), the digit 1 appears 3 times(Two times on the first number and another time on the third number), the digit 2 appears 2 times(One time on the second number and another time on the third number), and the digit 3 appears only one time, that is, on the last number.So the most common digit is 1. Case 2: For input given as: 3 111 222 5 The output of the program will be: 2 Description of the output: Both digits 1 and 2 appear the maximum number of times, that is, 3 times.Since 2 is bigger than 1, the answer is 2 instead of 1.
+// Write a function in JS fixage that receives an array of integers.The integers represent ages between 0 and 100. The function is required to join all the integers that are between 18 and 60 into a string separated with a comma and returns it.If there is no integer between 18 and 60, then the function returns "NA".For example: fixage([5, 15, 25, 78, 59, 45]) returns 25, 59, 45 and fixage([18, 3, 30, 22, 11, 60]) returns 18, 30, 22, 60 and fixage([1, 3, 3, 2, 11, 6]) returns NA
 
 class FixAge extends Component {
   state = {
     arrayTest: [1, 22, 30, 17, 18, 55, 44, 60, 422, 89, 100, 76, 61],
-    result: ""
+    arrayTestTwo: [1, 2, 3, 4, 5, 6, 7, 8],
+    result: "",
+    resultNaN: "",
+    NaN: "NaN"
   };
 
   getFixAge = array => {
-      return array.filter(arr => arr >= 18 && arr <= 60) + ", "
+    return array.filter(arr => arr >= 18 && arr <= 60);
   };
 
-  getArray = (array) => {
-      return array.map(el => el) + ""
-  }
+  getArray = array => {
+    return array.map(el => el) + "";
+  };
+
+  isFixed = array => {
+    if(this.state.result !== ""){
+      return this.state.NaN
+    }
+    return array.filter(arr => arr >= 18 && arr <= 60) + "" 
+  };
+
+  isFixedNan = array => {
+    if (this.state.resultNaN !== " ") {
+      return this.state.NaN;
+    }
+    return array.filter(arr => arr >= 18 && arr <= 60) + "" 
+  };
 
   componentDidMount() {
     this.setState({
-      result: this.getFixAge(this.state.arrayTest)
+      result: this.isFixed(this.state.arrayTest),
+      resultNaN: this.isFixedNan(this.state.arrayTestTwo)
     });
-    
   }
 
   render() {
+    let {result, resultNaN} = this.state
     return (
       <div className="fixage">
-        {console.log(this.getArray)}
         <p>Tablica - {this.getArray(this.state.arrayTest)}</p>
-        Fix Age = {this.state.result}
+        <p>Fix Age = {result}</p>
+        
+        <p>Tablica - {this.getArray(this.state.arrayTestTwo)}</p>
+        <p>Fix Age = {resultNaN}</p>
       </div>
     );
   }
